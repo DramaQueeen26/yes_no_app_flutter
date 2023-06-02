a253b0b7b6f485dc2e3899ad46c0f44d3afb9e00
Mariangel Yajure <mariangel.yajure@gmail.com>
2023-06-02 12:33:57 -0400

    Loading message

--- lib/presentation/widgets/chat/him_message_bubble.dart
+++ lib/presentation/widgets/chat/him_message_bubble.dart
@@ -40,9 +40,21 @@ class _ImageBubble extends StatelessWidget {
       borderRadius: BorderRadius.circular(20),
         child: Image.network(
           'https://yesno.wtf/assets/no/9-dc99c0e3c066b28d3a12262692cd5432.gif', 
-          width: size.width * 0.7, // * Colocamos el with del 70% con relación al teléfono
-          height: 150,
-          fit: BoxFit.cover
+          width: size.width * 0.8, // * Colocamos el width del 70% con relación al teléfono
+          height: 200,
+          fit: BoxFit.cover,
+          loadingBuilder: (context, child, loadingProgress) {
+            
+            if( loadingProgress == null ) return child;
+
+            return Container(
+              width: size.width * 0.8,
+              height: 200,
+              padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+              child: const Text('Mi amor está enviando una imagen'), 
+            );
+
+          },
           ),
       );
   }
